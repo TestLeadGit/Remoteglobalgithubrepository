e92f13a463725e48f950d7fe2605f605bc214b72
97sar <97sar@LAPTOP-EETDBIMU>
2025-07-25 23:17:59 +0530

    PUSHING FRAMEWORK TO GITHUB

---
+++ UFRAMEWORK_DESIGN_TL/src/test/java/Pac1/Normal_Framework.java
@@ -0,0 +1,12 @@
+package Pac1;
+
+import org.testng.annotations.Test;
+
+public class Normal_Framework 
+{
+	@Test
+	public void TC1()
+	{
+		System.out.println("This is a Normal Framework");
+	}
+}
