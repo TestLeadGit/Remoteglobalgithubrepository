70f0c84cfa236c8e369000cc90d14515987b24cf
97sar <97sar@LAPTOP-EETDBIMU>
2025-07-26 01:01:58 +0530

    Fork_pullrequest_class added

---
+++ UFRAMEWORK_DESIGN_TL/src/test/java/Pac1/Fork_pullrequest_class.java
@@ -0,0 +1,13 @@
+package Pac1;
+
+import org.testng.annotations.Test;
+
+public class Fork_pullrequest_class 
+{
+	@Test
+	public void Fork_pullrequest_class()
+	{
+		System.out.println("Fork_pullrequest_class");
+	}
+
+}
